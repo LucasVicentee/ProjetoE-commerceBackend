57b8970a49e180caf33f4c7283cb4731d3291302
LucasVicentee <lh201612@gmail.com>
2025-11-14 20:25:36 -0300

    inicializando o Projeto

---
+++ src/test/java/com/LucasVicentee/ProjetoEcommerce/ProjetoEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LucasVicentee.ProjetoEcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
